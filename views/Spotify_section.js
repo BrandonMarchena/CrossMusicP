8f97893d99a58e898149e4836d648d8e675a87b3
BrandonMarchena <brandon.marchena@aplinet.co>
2020-11-22 05:38:12 -0500

    Agregando paquete para visualizar archivos env y cifrar ciertos datos

--- views/Spotify_section.js
+++ views/Spotify_section.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
 SafeAreaView,
 StyleSheet,
@@ -6,12 +6,18 @@ ScrollView,
 View,
 Text,
 StatusBar,
+Linking,
 } from 'react-native';
+import axios from 'axios';
+import {SPOTIFY_CLIENT_SECRET} from '@env'
 
 const Spotify_section = () => {
+
+
     return (
         <>
             <Text>Spotify Secction</Text>
+            <Text>{SPOTIFY_CLIENT_SECRET}</Text>
         </>
     );
 }
